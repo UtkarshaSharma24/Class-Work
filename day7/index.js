827d45955ce76bab9bd4f3e492fc210ea88ff9c1
UtkarshaSharma24 <23utkarsh4.0@gmail.com>
2025-09-18 02:43:39 +0530

    message

---
+++ day7/index.js
@@ -0,0 +1,23 @@
+let name = "xus";
+const p1 = 3.14;
+console.log("let and const in block")
+console.log("value of name in block scope", name)
+console.log("value of pi in blockscope",pi)
+
+{
+let name = "xus";
+const p1 = 3.14;
+console.log("let and const in block")
+console.log("value of name in block scope", name)
+console.log("value of pi in blockscope",pi)
+}
+
+
+function getValue(){
+let name = "xus";
+const p1 = 3.14;
+console.log("let and const in block")
+console.log("value of name in block scope", name)
+console.log("value of pi in blockscope",pi)
+}
+getValue();
