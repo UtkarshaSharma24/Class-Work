484d10ca96804f33d07b70a62355ce5865b0ace7
Utkarsh Sharma <153360041+UtkarshaSharma24@users.noreply.github.com>
2025-10-16 15:25:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Monday: Math</li>
+        <li>Tuesday: Science</li>
+        <li>Wednesday: English</li>
+        <li>Thursday: History</li>
+        <li>Friday: Computer</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-{/*<div idn="outer-div">
-    <div id= "inner-div">
-        <h1 id="heading"></h1>
-        <h1 id="heading2"></h1>
-        <h1 id="heading3"></h1>
-        <h1 id="heading4"></h1>
-    </div>
-    </div>*/}
-  const heading = React.createElement("div", {id:"outer-div"}, 
-    React.createElement("div", {id:"inner-div"},
-        [React.createElement("h1", {id:"heading"}, "Hello World from React"),
-        React.createElement("h1", {id:"heading2"}, "Hello World from React"),
-        React.createElement("h1", {id:"heading3"}, "Hello World from React"),
-        React.createElement("h1", {id:"heading4"}, "Hello World from React")]
-    )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Header';
+import App from './App';
+import './index.css'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Header />   {/* <-- Add this */}
+    <App />
+  </React.StrictMode>
 );
-  console.log(heading);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
